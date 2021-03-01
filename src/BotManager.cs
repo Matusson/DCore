adc3bc06d5824d9b20cd05f65a2808b757a61265
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-01 19:46:36 +0100

    Finish LoadAccounts()

--- src/BotManager.cs
+++ src/BotManager.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DCore.Structs;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace DCore
@@ -10,13 +12,15 @@ namespace DCore
     /// </summary>
     public class BotManager
     {
+        private List<TokenInfo> _tokens = new List<TokenInfo>();
+
         /// <summary>
         /// Loads ID and token information from a .txt file.
         /// </summary>
         /// <param name="pathToFile"> The path to the .txt file containing data. </param>
         /// <exception cref="FileNotFoundException"> Thrown when <paramref name="pathToFile"/> does not exist. </exception>
-        /// <exception cref="ArgumentException"> Thrown when a file is empty or incorrectly formatted. </exception>
-        /// <returns> The amount of bots that were loaded. </returns>
+        /// <exception cref="ArgumentException"> Thrown when the file is empty or incorrectly formatted. </exception>
+        /// <returns> The amount of accounts that were loaded. </returns>
         public int LoadAccounts(string pathToFile)
         {
             if (File.Exists(pathToFile))
@@ -29,6 +33,7 @@ namespace DCore
             if (lines.Length == 0)
                 throw new ArgumentException("The specified file was empty.");
 
+            int addedTokens = 0;
             string incorrectlyFormattedException = "File was incorrectly formatted. Make sure that each line starts with the ID " +
                         "followed by a token, separated by a space.";
 
@@ -48,7 +53,21 @@ namespace DCore
 
                 //The second value should be a string
                 string token = perLineInformation[1];
+
+
+                TokenInfo tokenInfo = new TokenInfo(id, token);
+
+                //Only add the token if it doesn't already exist
+                if (!_tokens.Any(x => x.id == tokenInfo.id))
+                {
+                    _tokens.Add(tokenInfo);
+                    addedTokens++;
+                }
             }
+
+            return addedTokens;
         }
+
+
     }
 }
