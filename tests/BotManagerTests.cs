8314a4fcc6074d7be60a26799947808c2c602eae
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-01 19:53:05 +0100

    Add test class

---
+++ tests/BotManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore.Tests
+{
+    [TestClass()]
+    public class BotManagerTests
+    {
+        [TestMethod()]
+        public void LoadAccountsTest()
+        {
+            
+        }
+    }
+}
