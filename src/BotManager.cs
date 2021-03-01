f6158efca43fdb1157aedb0d276ef548e4c237fb
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-01 19:10:49 +0100

    Declare BotManager and DiscordBot

---
+++ src/BotManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore
+{
+    /// <summary>
+    /// Manages <see cref="DiscordBot"/> accounts.
+    /// </summary>
+    public class BotManager
+    {
+
+    }
+}
---
+++ src/DiscordBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore
+{
+    /// <summary>
+    /// Represents a single Discord bot account.
+    /// </summary>
+    public class DiscordBot
+    {
+
+    }
+}
