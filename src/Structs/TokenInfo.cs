1bea4425f0628d00546ce62afcde02e9ecd81828
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-01 19:39:29 +0100

    Add TokenInfo struct

---
+++ src/Structs/TokenInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore.Structs
+{
+    /// <summary>
+    /// Stores information related to a <see cref="DiscordBot"/> token.
+    /// </summary>
+    struct TokenInfo
+    {
+        /// <summary>
+        /// The ID of the <see cref="DiscordBot"/>.
+        /// </summary>
+        public ulong id;
+
+        /// <summary>
+        /// The token for the <see cref="DiscordBot"/>.
+        /// </summary>
+        public string token;
+
+        /// <summary>
+        /// Constructs a <see cref="TokenInfo"/>.
+        /// </summary>
+        /// <param name="id"> <inheritdoc cref="id"/> </param>
+        /// <param name="token"> <inheritdoc cref="token"/> </param>
+        public TokenInfo(ulong id, string token)
+        {
+            this.id = id;
+            this.token = token;
+        }
+    }
+}
