4c5973bbc593c3ccfacf44154f4507d223b31021
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-02 20:04:26 +0100

    Add StartAsync()

--- src/DiscordBot.cs
+++ src/DiscordBot.cs
@@ -1,7 +1,9 @@
 ﻿using DCore.Structs;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DCore
 {
@@ -11,9 +13,30 @@ namespace DCore
     public class DiscordBot
     {
         /// <summary>
+        /// The underlying Discord.NET client.
+        /// </summary>
+        public DiscordSocketClient Client { get; set; }
+
+        /// <summary>
         /// The ID and token information for <see cref="DiscordBot"/>.
         /// </summary>
         public TokenInfo TokenInfo { get; set; }
+
+
+        public async Task StartAsync(DiscordSocketConfig config = null)
+        {
+            //If config is null, use the default one
+            if (config == null)
+                config = new DiscordSocketConfig();
+
+            //Initialize the client
+            if (Client == null)
+                Client = new DiscordSocketClient(config);
+
+            //Log in and start
+            await Client.LoginAsync(Discord.TokenType.Bot, TokenInfo.token);
+            await Client.StartAsync();
+        }
 
         /// <summary>
         /// Constructs a new <see cref="DiscordBot"/>.
