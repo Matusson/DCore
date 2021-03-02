89a86b60d5957af1a539b3fd29f2d0ce8d7c7ed9
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-02 16:38:51 +0100

    Add tests for DCoreExtensions.LoadAccountsFromFile

--- src/DCoreExtensions.cs
+++ src/DCoreExtensions.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Text;
 using DCore.Helpers;
 
-namespace DCore.Extensions
+namespace DCore
 {
-    static class DCoreExtensions
+    public static class DCoreExtensions
     {
         /// <summary>
         /// <inheritdoc cref="BotAccountLoader.LoadAccountsFromFile(string)"/>
