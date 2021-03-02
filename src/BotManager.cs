1c8201ac840396b31d31046ccc5389babd26b230
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-02 16:02:46 +0100

    Move LoadAccounts to BotAccountLoader

--- src/BotManager.cs
+++ src/BotManager.cs
@@ -1,4 +1,5 @@
-﻿using DCore.Structs;
+﻿using DCore.Helpers;
+using DCore.Structs;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -46,49 +47,14 @@ namespace DCore
         /// <returns> The amount of accounts that were loaded. </returns>
         public int LoadAccounts(string pathToFile)
         {
-            if (!File.Exists(pathToFile))
-                throw new FileNotFoundException("The specified file does not exist.");
+            BotAccountLoader loader = new BotAccountLoader();
+            var tokens = loader.LoadAccountsFromFile(pathToFile);
 
-            //Open the file
-            string[] lines = File.ReadAllLines(pathToFile);
+            //Ensure only unique information is loaded
+            tokens = tokens.Except(_tokens).ToList();
 
-            //If the file was empty
-            if (lines.Length == 0)
-                throw new ArgumentException("The specified file was empty.");
-
-            int addedTokens = 0;
-            string incorrectlyFormattedException = "File was incorrectly formatted. Make sure that each line starts with the ID " +
-                        "followed by a token, separated by a space.";
-
-            //In every line, there should be an ulong ID and a string TOKEN separated by a space
-            for (int i = 0; i < lines.Length; i++)
-            {
-                string line = lines[i].Trim();
-                string[] perLineInformation = line.Split(' ', ',');
-
-                //Throw an exception if the data is incorrectly formatted
-                if (perLineInformation.Length != 2)
-                    throw new ArgumentException(incorrectlyFormattedException);
-
-                //The first value should be an ULONG
-                if (!ulong.TryParse(perLineInformation[0], out ulong id))
-                    throw new ArgumentException(incorrectlyFormattedException);
-
-                //The second value should be a string
-                string token = perLineInformation[1];
-
-
-                TokenInfo tokenInfo = new TokenInfo(id, token);
-
-                //Only add the token if it doesn't already exist
-                if (!_tokens.Any(x => x.id == tokenInfo.id))
-                {
-                    _tokens.Add(tokenInfo);
-                    addedTokens++;
-                }
-            }
-
-            return addedTokens;
+            _tokens.AddRange(tokens);
+            return tokens.Count;
         }
 
 
