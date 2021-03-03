fd4d74c47edc22447d33fe3254bf1970edc96c8d
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-03 15:26:47 +0100

    Add gitignore for tests

---
+++ tests/DiscordBotTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore.Tests
+{
+    /* WARNING!
+     * This test class requires an ID and token to be provided in the TestToken.txt file.
+     * Otherwise the bot will not establish a connection and the tests will fail.
+     */
+
+    [TestClass()]
+    [DeploymentItem("TestToken.txt")]
+    public class DiscordBotTests
+    {
+        [TestMethod()]
+        public void StartAsync_NoConfig()
+        {
+            BotManager manager = new BotManager();
+            manager.LoadAccountsFromFile("TestToken.txt");
+
+        }
+    }
+}
