ae09d1b1c5fae6a4b641a9583a1c0252749b4641
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-04 19:20:40 +0100

    Add DCoreLogger.cs and LogType.cs

---
+++ src/DCoreLogger.cs
@@ -0,0 +1,15 @@
+﻿using DCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore
+{
+    public class DCoreLogger
+    {
+        public void LogInformation(LogType type, string message)
+        {
+
+        }
+    }
+}
