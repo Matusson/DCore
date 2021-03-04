228c89b3e0076bf518c9b49012df3d754e13e0f6
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-04 19:37:27 +0100

    Add LoggingHelper

---
+++ src/Helpers/LoggingHelper.cs
@@ -0,0 +1,20 @@
+﻿using DCore.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore.Helpers
+{
+    internal class LoggingHelper
+    {
+        internal void WriteToConsole(LogType type, string toWrite)
+        {
+
+        }
+
+        internal void WriteToFile(LogType type, string toWrite)
+        {
+
+        }
+    }
+}
