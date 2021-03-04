fdc29a5f08f07c98dfff36b8782b81ca0ae5cc4c
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-04 19:56:23 +0100

    Add functionality for GetPrefix

--- src/Helpers/LoggingWriter.cs
+++ src/Helpers/LoggingWriter.cs
@@ -1,4 +1,5 @@
 ﻿using DCore.Enums;
+using Pastel;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -17,9 +18,50 @@ namespace DCore.Helpers
 
         }
 
+        /// <summary>
+        /// Gets the prefix for type, optionally colored.
+        /// </summary>
+        /// <param name="type"> The type of log. </param>
+        /// <param name="colored"> Should the prefix be colored? </param>
+        /// <returns> The prefix to use for this log type. </returns>
         private string GetPrefix(LogType type, bool colored = false)
         {
-            
+            string prefix = "";
+            switch (type)
+            {
+                case LogType.Debug:
+                    prefix = "[DEBUG]";
+                    if (colored)
+                        prefix = prefix.Pastel("#e3d76b");
+                    break;
+
+                case LogType.Info:
+                    prefix = "[INFO]";
+                    break;
+
+                case LogType.Warn:
+                    prefix = "[WARN]";
+                    if (colored)
+                        prefix = prefix.Pastel("#eda334");
+                    break;
+
+                case LogType.Error:
+                    prefix = "[ERROR]";
+                    if (colored)
+                        prefix = prefix.Pastel("#eb4b36");
+                    break;
+
+                case LogType.CriticalError:
+                    prefix = "[DEBUG]";
+                    if (colored)
+                        prefix = prefix.Pastel("#9e1006");
+                    break;
+
+                default:
+                    return null;
+            }
+
+            return prefix;
         }
     }
 }
