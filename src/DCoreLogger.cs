3ba5cff5d4698aed9e111e744270f4a695316ad9
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-05 17:30:55 +0100

    Add shorthand methods

--- src/DCoreLogger.cs
+++ src/DCoreLogger.cs
@@ -8,8 +8,108 @@ using System.Threading.Tasks;
 
 namespace DCore
 {
+    /// <summary>
+    /// Handles logging into console and files.
+    /// </summary>
     public class DCoreLogger
     {
+        /// <summary>
+        /// Logs information to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Info(string message)
+        {
+            LogInformation(LogType.Info, message);
+        }
+
+        /// <summary>
+        /// Logs information to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Info(object message)
+        {
+            LogInformation(LogType.Info, message.ToString());
+        }
+
+        /// <summary>
+        /// Logs debug information to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Debug(string message)
+        {
+            LogInformation(LogType.Debug, message);
+        }
+
+        /// <summary>
+        /// Logs debug information to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Debug(object message)
+        {
+            LogInformation(LogType.Debug, message.ToString());
+        }
+
+        /// <summary>
+        /// Logs a warning to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Warn(string message)
+        {
+            LogInformation(LogType.Warn, message);
+        }
+
+        /// <summary>
+        /// Logs a warning to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Warn(object message)
+        {
+            LogInformation(LogType.Warn, message.ToString());
+        }
+
+        /// <summary>
+        /// Logs an error to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Error(string message)
+        {
+            LogInformation(LogType.Error, message);
+        }
+
+        /// <summary>
+        /// Logs an error to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void Error(object message)
+        {
+            LogInformation(LogType.Error, message.ToString());
+        }
+
+        /// <summary>
+        /// Logs a critical error to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void CriticalError(string message)
+        {
+            LogInformation(LogType.CriticalError, message);
+        }
+
+        /// <summary>
+        /// Logs a critical error to console.
+        /// </summary>
+        /// <param name="message"> The message to log. </param>
+        public void CriticalError(object message)
+        {
+            LogInformation(LogType.CriticalError, message.ToString());
+        }
+
+
+
+        /// <summary>
+        /// Logs a message of type <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type"> Type of log. </param>
+        /// <param name="message"> Message to log. </param>
         public void LogInformation(LogType type, string message)
         {
             //TODO:Make this configurable
