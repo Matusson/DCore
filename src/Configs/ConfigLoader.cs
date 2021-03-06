4a02fe552b286f728ef3c531eab6d7c534118083
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-06 10:52:28 +0100

    Move helper functions to ConfigLoader.cs

---
+++ src/Configs/ConfigLoader.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DCore.Configs
+{
+    /// <summary>
+    /// Helper class for loading configs.
+    /// </summary>
+    internal class ConfigLoader
+    {
+        private readonly DCoreConfig _dcoreConfig;
+
+        /// <summary>
+        /// Loads a config of <see cref="Type"/> <paramref name="type"/> from specified path.
+        /// </summary>
+        /// <param name="path"> The path to load the config file from. </param>
+        /// <param name="type"> The <see cref="Type"/> of the config class. </param>
+        /// <exception cref="FileNotFoundException"> Thrown when file is not found. </exception>
+        /// <returns></returns>
+        internal object LoadConfig(string path, Type type)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("File does not exist.");
+
+            //Attempt to load the file
+            string content = File.ReadAllText(path);
+            object config = JsonConvert.DeserializeObject(content, type);
+
+            return config;
+        }
+
+        /// <summary>
+        /// Gets the path to the global config file.
+        /// </summary>
+        /// <returns> The path to the global config file. </returns>
+        internal string GetPathToGlobalConfig()
+        {
+            return Path.Combine(_dcoreConfig.ConfigPath, "global.json");
+        }
+
+        /// <summary>
+        /// Constructs a ConfigLoader with the specified config.
+        /// </summary>
+        /// <param name="dcoreConfig"></param>
+        internal ConfigLoader(DCoreConfig dcoreConfig)
+        {
+            _dcoreConfig = dcoreConfig;
+        }
+    }
+}
--- src/Configs/ConfigManager.cs
+++ src/Configs/ConfigManager.cs
@@ -21,7 +21,10 @@ namespace DCore.Configs
             {
                 //Load the config if needed
                 if (_config == null)
-                    _config = LoadConfig(GetPathToGlobalConfig(), typeof(GlobalBotConfig)) as GlobalBotConfig;
+                {
+                    _config = _loader.LoadConfig(
+                        _loader.GetPathToGlobalConfig(), typeof(GlobalBotConfig)) as GlobalBotConfig;
+                }
 
                 return _config;
             }
@@ -32,45 +35,16 @@ namespace DCore.Configs
             }
         }
         private GlobalBotConfig _config;
+        private readonly ConfigLoader _loader;
 
-        private readonly DCoreConfig _dcoreConfig;
-
-        /// <summary>
-        /// Loads a config of <see cref="Type"/> <paramref name="type"/> from specified path.
-        /// </summary>
-        /// <param name="path"> The path to load the config file from. </param>
-        /// <param name="type"> The <see cref="Type"/> of the config class. </param>
-        /// <exception cref="FileNotFoundException"> Thrown when file is not found. </exception>
-        /// <returns></returns>
-        private object LoadConfig(string path, Type type)
-        {
-            if (!File.Exists(path))
-                throw new FileNotFoundException("File does not exist.");
-
-            //Attempt to load the file
-            string content = File.ReadAllText(path);
-            object config = JsonConvert.DeserializeObject(content, type);
-
-            return config;
-        }
-
-        /// <summary>
-        /// Gets the path to the global config file.
-        /// </summary>
-        /// <returns> The path to the global config file. </returns>
-        private string GetPathToGlobalConfig()
-        {
-            return Path.Combine(_dcoreConfig.ConfigPath, "global.json");
-        }
-
-
+        
         /// <summary>
         /// Constructs a <see cref="ConfigManager"/> with the specified DCore config.
         /// </summary>
         /// <param name="dcoreConfig"></param>
         public ConfigManager(DCoreConfig dcoreConfig)
         {
-            _dcoreConfig = dcoreConfig;
+            _loader = new ConfigLoader(dcoreConfig);
         }
     }
 }
