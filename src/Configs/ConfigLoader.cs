caccb38525149a289b8a2c3107c8da5847a7449f
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-06 10:18:34 +0100

    Add ConfigLoader

---
+++ src/Configs/ConfigLoader.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DCore.Configs
+{
+    internal class ConfigLoader
+    {
+        /// <summary>
+        /// Loads a config of <see cref="Type"/> <paramref name="type"/> from specified path.
+        /// </summary>
+        /// <param name="path"> The path to load the config file from. </param>
+        /// <param name="type"> The <see cref="Type"/> of the config class. </param>
+        /// <returns></returns>
+        internal object LoadConfig(string path, Type type)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("File does not exist.");
+
+            //Attempt to load the file
+            string content = File.ReadAllText(path);
+            object config = JsonConvert.DeserializeObject(content, type);
+
+            return config;
+        }
+    }
+}
