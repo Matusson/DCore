8c2fc87059dc112d9a823032ca82644fa2399fc8
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-08 19:52:38 +0100

    Add MultilanguageManager and LanguageData

---
+++ src/Multilanguage/MultilanguageManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DCore
+{
+    /// <summary>
+    /// Manages multi-language data.
+    /// </summary>
+    public class MultilanguageManager
+    {
+        public void LoadLanguageData()
+        {
+
+        }
+
+        public void GetString()
+        {
+
+        }
+    }
+}
