5c1380652c5d1d810ad94b155b25208f55cefe5f
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-10 16:29:57 +0100

    Add EmptyPath test to LoadLanguageData

---
+++ tests/Multilanguage/MultilanguageManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DCore.Configs;
+using Moq;
+using System.IO;
+using System.Linq;
+
+namespace DCore.Tests
+{
+    [TestClass()]
+    public class MultilanguageManagerTests
+    {
+        [TestMethod()]
+        public void LoadLanguageData_EmptyPath()
+        {
+            DCoreConfig config = new DCoreConfig();
+
+            //Clear the files if any exist
+            var oldFiles = Directory.EnumerateFiles(config.LanguagesPath).ToList();
+            oldFiles.ForEach(x => File.Delete(x));
+
+            Mock<DiscordBot> bot = new Mock<DiscordBot>();
+            bot.SetupGet(x => x.Config.Language).Returns("en");
+            MultilanguageManager manager = new MultilanguageManager(bot.Object, config);
+
+            manager.LoadLanguageData();
+
+            int fileCount = Directory.EnumerateFiles(config.LanguagesPath).ToList().Count;
+            string expectedFile = Path.Combine(config.LanguagesPath, "example.json");
+            Assert.IsTrue(fileCount == 1 
+                && File.Exists(expectedFile));
+        }
+    }
+}
