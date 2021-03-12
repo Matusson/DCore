7f50bc10a64b5ebc37335c2596b7b8ff452db90a
Matusson <41548922+Matusson@users.noreply.github.com>
2021-03-12 18:33:01 +0100

    Add ConfirmWithReact extension

---
+++ src/Extensions/IUserMessageExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCore
+{
+    /// <summary>
+    /// Handles extension methods for IUserMessage from Discord.NET.
+    /// </summary>
+    public static class IUserMessageExtensions
+    {
+        /// <summary>
+        /// Adds a confirmation emoji to the <paramref name="message"/>.
+        /// </summary>
+        /// <param name="message"> The message to add the reaction to. </param>
+        /// <param name="bot"> The bot to use the emoji from. </param>
+        public static void ConfirmWithReact(this IUserMessage message, DiscordBot bot)
+        {
+            IEmote emote = bot.Config.ConfirmationEmoji;
+            _ = Task.Run(() => message.AddReactionAsync(emote));
+        }
+    }
+}
